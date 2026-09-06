761eb481fc582a65f59593c3ce7e725089ae979a
Shubham Kumar <iamkumarshubham17@gmail.com>
2026-09-06 09:11:42 +0530

    connected mySql
    # Conflicts:
    #	java-hibernate-app/crudapp/crudapp/src/main/resources/application.properties

--- java-hibernate-app/crudapp/crudapp/src/main/java/com/example/crudapp/CrudappApplication.java
+++ java-hibernate-app/crudapp/crudapp/src/main/java/com/example/crudapp/CrudappApplication.java
@@ -1,7 +1,9 @@
 package com.example.crudapp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudappApplication {
@@ -10,4 +12,12 @@ public class CrudappApplication {
 		SpringApplication.run(CrudappApplication.class, args);
 	}
 
+
+	@Bean
+	public CommandLineRunner clr(String[] args) {
+		return runner -> {
+			System.out.println("Hello World");
+		};
+
+	}
 }
